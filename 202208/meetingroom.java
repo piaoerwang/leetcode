d887a8964728c187e6ac1e379f8cd335fd24e99c
wangyuwei <wang5439@umn.edu>
2022-09-05 09:17:30 -0700

    2022 08 lc

---
+++ 202208/meetingroom.java
@@ -0,0 +1,25 @@
+public class Solution {
+	public int minMeetingRooms(int[][] intervals) {
+    Arrays.sort(intervals, Comparator.comparing((int[] itv) -> itv[0]));
+ 
+    PriorityQueue<Integer> heap = new PriorityQueue<>();
+    int count = 0;
+    for (int[] itv : intervals) {
+        if (heap.isEmpty()) {
+            count++;
+            heap.offer(itv[1]);
+        } else {
+            if (itv[0] >= heap.peek()) {
+                heap.poll();
+            } else {
+                count++;
+            }
+ 
+            heap.offer(itv[1]);
+        }
+    }
+ 
+    return count;
+}
+
+}
